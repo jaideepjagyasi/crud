7587a194044d133ef487e425baa92857647b68db
Shahzeb Qureshi <38107578+ssq7@users.noreply.github.com>
2019-02-17 19:35:06 +0530

    Make Normal Crud Application In PHP

---
+++ add.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <title>Add Data</title>
+</head>
+ 
+<body>
+<?php
+//including the database connection file
+include_once("config.php");
+ 
+if(isset($_POST['Submit'])) {    
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+        
+    // checking empty fields
+    if(empty($name) || empty($age) || empty($email) || empty($number)) {                
+        if(empty($name)) {
+            echo "<font color='red'>Name field is empty.</font><br/>";
+        }
+        
+        if(empty($age)) {
+            echo "<font color='red'>Age field is empty.</font><br/>";
+        }
+        
+        if(empty($email)) {
+            echo "<font color='red'>Email field is empty.</font><br/>";
+        }
+        if(empty($number)) {
+            echo "<font color='red'>Number field is empty.</font><br/>";
+        }
+        
+        //link to the previous page
+        echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+    } else { 
+        // if all the fields are filled (not empty)             
+        //insert data to database
+        $result = mysqli_query($mysqli, "INSERT INTO users(name,age,email,number) VALUES('$name','$age','$email','$number')");
+        
+        //display success message
+        echo "<font color='green'>Data added successfully.";
+        echo "<br/><a href='index.php'>View Result</a>";
+    }
+}
+?>
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$databaseHost = 'localhost';
+$databaseName = 'crud';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
+        
